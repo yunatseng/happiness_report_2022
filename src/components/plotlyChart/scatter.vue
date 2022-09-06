f5e4902bc5c0c7c7e53f855a3df75e429ad92831
yunaTseng <fav0124@gmail.com>
2022-09-06 23:42:52 +0800

    ADD: income scatter plot

--- src/components/plotlyChart/scatter.vue
+++ src/components/plotlyChart/scatter.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-    <div id="myDiv"></div>
+    <div id="scatterAll"></div>
+    <div style="display: flex; justify-content: space-evenly">
+      <div id="High"></div>
+      <div id="Middle"></div>
+      <div id="Low"></div>
+    </div>
+
     <div class="mx-5 mx-md-14 text-center">
-      <v-expansion-panels >
+      <v-expansion-panels>
         <v-expansion-panel>
-          <v-expansion-panel-header style="font-size:19px;">Correlation Heatmap<span class="cyan--text" style="font-size:16px;">（可點我對照上圖）</span></v-expansion-panel-header>
+          <v-expansion-panel-header style="font-size: 19px"
+            >Correlation Heatmap<span class="cyan--text" style="font-size: 16px"
+              >（可點我對照上圖）</span
+            ></v-expansion-panel-header
+          >
           <v-expansion-panel-content>
-            <img style="width:850px;margin-left:-100px;" src="../../assets/heatmap_all.png" alt="Correlation Heatmap for World Happiness Report 2022">
+            <img
+              style="width: 850px; margin-left: -100px"
+              src="../../assets/heatmap_all.png"
+              alt="Correlation Heatmap for World Happiness Report 2022"
+            />
           </v-expansion-panel-content>
         </v-expansion-panel>
       </v-expansion-panels>
@@ -15,105 +29,22 @@
 </template>
 
 <script>
-import Plotly from "plotly.js-dist-min";
-import happy_df_without_rank from "../../assets/data/happy_df_without_rank.csv";
+import GDP_high from "../../assets/data/GDP_high.csv";
+import GDP_mid from "../../assets/data/GDP_mid.csv";
+import GDP_low from "../../assets/data/GDP_low.csv";
+
+import { scatterAll } from "./utils";
+import { GDPScatter } from "./utils";
 
 export default {
   data() {
-    return {
-      
-    };
+    return {};
   },
   mounted() {
-    const rows = happy_df_without_rank;
-
-    function unpack(rows, key) {
-      return rows.map(function (row) {
-        return row[key];
-      });
-    }
-
-    const pl_colorscale = [
-      [0.0, "#263238"],
-      [0.2, "#263238"],
-      [0.3, "#455A64"],
-      [0.4, "#78909C"],
-      [0.5, "#80DEEA"],
-      [0.6, "#4DD0E1"],
-      [0.666, "#00BCD4"],
-      [0.8, "#0052d4"],
-      [1, "#0052d4"],
-    ];
-
-    const axis = () => ({
-      showline: false,
-      zeroline: false,
-      gridcolor: "#ffff",
-      ticklen: 2,
-      tickfont: { size: 10 },
-      titlefont: { size: 12 },
-    });
-
-    const data = [
-      {
-        type: "splom",
-        dimensions: [
-          { label: "Happiness", values: unpack(rows, "Happiness score") },
-          { label: "GDP / Capita", values: unpack(rows, "GDP per capita") },
-          { label: "Social support", values: unpack(rows, "Social support") },
-          {
-            label: "Healthy life",
-            values: unpack(rows, "Healthy life expectancy"),
-          },
-          {
-            label: "Freedom life",
-            values: unpack(rows, "Freedom to make life choices"),
-          },
-          { label: "Generosity", values: unpack(rows, "Generosity") },
-          {
-            label: "Corruption",
-            values: unpack(rows, "Perceptions of corruption"),
-          },
-        ],
-        text: unpack(rows, "Country"),
-        marker: {
-          color: unpack(rows, "Happiness score"),
-          colorscale: pl_colorscale,
-          size: 4,
-          line: {
-            color: "white",
-            width: 0.5,
-          },
-        },
-      },
-    ];
-
-    const layout = {
-      title:
-        "Scatterplot Matrix (SPLOM) for World Happiness Report 2022<br><span style='font-size:12px;color:grey;'>Data source: <a href='https://www.kaggle.com/datasets/ajaypalsinghlo/world-happiness-report-2022'>https://www.kaggle.com/datasets/ajaypalsinghlo/world-happiness-report-2022</a></span>",
-      height: 1100,
-      width: 1100,
-      autosize: false,
-      hovermode: "closest",
-      dragmode: "select",
-      plot_bgcolor: "rgba(240,240,240, 0.95)",
-      xaxis: axis(),
-      yaxis: axis(),
-      xaxis2: axis(),
-      xaxis3: axis(),
-      xaxis4: axis(),
-      xaxis5: axis(),
-      xaxis6: axis(),
-      xaxis7: axis(),
-      yaxis2: axis(),
-      yaxis3: axis(),
-      yaxis4: axis(),
-      yaxis5: axis(),
-      yaxis6: axis(),
-      yaxis7: axis(),
-    };
-
-    Plotly.newPlot("myDiv", data, layout);
+    scatterAll();
+    GDPScatter(GDP_high, "High");
+    GDPScatter(GDP_mid, "Middle");
+    GDPScatter(GDP_low, "Low");
   },
 };
 </script>
---
+++ src/components/plotlyChart/utils.js
@@ -0,0 +1,165 @@
+import Plotly from "plotly.js-dist-min";
+import happy_df_without_rank from "../../assets/data/happy_df_without_rank.csv";
+
+
+export function scatterAll() {
+    // Initialize the chart
+    const rows = happy_df_without_rank;
+
+    function unpack(rows, key) {
+        return rows.map(function (row) {
+            return row[key];
+        });
+    }
+
+    const pl_colorscale = [
+        [0.0, "#263238"],
+        [0.2, "#263238"],
+        [0.3, "#455A64"],
+        [0.4, "#78909C"],
+        [0.5, "#80DEEA"],
+        [0.6, "#4DD0E1"],
+        [0.666, "#00BCD4"],
+        [0.8, "#0052d4"],
+        [1, "#0052d4"],
+    ];
+
+    const axis = () => ({
+        showline: false,
+        zeroline: false,
+        gridcolor: "#ffff",
+        ticklen: 2,
+        tickfont: { size: 10 },
+        titlefont: { size: 12 },
+    });
+
+    const data = [
+        {
+            type: "splom",
+            dimensions: [
+                { label: "Happiness", values: unpack(rows, "Happiness score") },
+                { label: "GDP / Capita", values: unpack(rows, "GDP per capita") },
+                { label: "Social support", values: unpack(rows, "Social support") },
+                {
+                    label: "Healthy life",
+                    values: unpack(rows, "Healthy life expectancy"),
+                },
+                {
+                    label: "Freedom life",
+                    values: unpack(rows, "Freedom to make life choices"),
+                },
+                { label: "Generosity", values: unpack(rows, "Generosity") },
+                {
+                    label: "Corruption",
+                    values: unpack(rows, "Perceptions of corruption"),
+                },
+            ],
+            text: unpack(rows, "Country"),
+            marker: {
+                color: unpack(rows, "Happiness score"),
+                colorscale: pl_colorscale,
+                size: 4,
+                line: {
+                    color: "white",
+                    width: 0.5,
+                },
+            },
+        },
+    ];
+
+    const layout = {
+        title:
+            "Scatterplot Matrix (SPLOM) for World Happiness Report 2022<br><span style='font-size:12px;color:grey;'>Data source: <a href='https://www.kaggle.com/datasets/ajaypalsinghlo/world-happiness-report-2022'>https://www.kaggle.com/datasets/ajaypalsinghlo/world-happiness-report-2022</a></span>",
+        height: 1100,
+        width: 1100,
+        autosize: false,
+        hovermode: "closest",
+        dragmode: "select",
+        plot_bgcolor: "rgba(240,240,240, 0.95)",
+        xaxis: axis(),
+        yaxis: axis(),
+        xaxis2: axis(),
+        xaxis3: axis(),
+        xaxis4: axis(),
+        xaxis5: axis(),
+        xaxis6: axis(),
+        xaxis7: axis(),
+        yaxis2: axis(),
+        yaxis3: axis(),
+        yaxis4: axis(),
+        yaxis5: axis(),
+        yaxis6: axis(),
+        yaxis7: axis(),
+    };
+
+    Plotly.newPlot("scatterAll", data, layout);
+}
+
+export function GDPScatter(csv, name) {
+
+    const rows = csv;
+
+    function unpack(rows, key) {
+        return rows.map(function (row) {
+            return row[key];
+        });
+    }
+
+    const pl_colorscale = [
+        [0.0, "#263238"],
+        [0.2, "#263238"],
+        [0.3, "#455A64"],
+        [0.4, "#78909C"],
+        [0.5, "#80DEEA"],
+        [0.6, "#4DD0E1"],
+        [0.666, "#00BCD4"],
+        [0.8, "#0052d4"],
+        [1, "#0052d4"],
+    ];
+
+    const axis = () => ({
+        showline: false,
+        zeroline: false,
+        gridcolor: "#ffff",
+        ticklen: 2,
+        tickfont: { size: 10 },
+        titlefont: { size: 12 },
+    });
+
+    const data = [
+        {
+            type: "splom",
+            dimensions: [
+                { label: "Happiness", values: unpack(rows, "Happiness score") },
+                { label: "GDP / Capita", values: unpack(rows, "GDP per capita") },
+            ],
+            text: unpack(rows, "Country"),
+            marker: {
+                color: unpack(rows, "Happiness score"),
+                colorscale: pl_colorscale,
+                size: 6,
+                line: {
+                    color: "white",
+                    width: 0.5,
+                },
+            },
+        },
+    ];
+
+    const layout = {
+        title:
+            `Happiness / ${name}-income<br><span style='font-size:11px;color:grey;'>Ref: <a href='https://data.worldbank.org/income-level/${name}-income'>https://data.worldbank.org/income-level/${name}-income</a></span>`,
+        height: 380,
+        width: 380,
+        autosize: false,
+        hovermode: "closest",
+        dragmode: "select",
+        plot_bgcolor: "rgba(240,240,240, 0.95)",
+        xaxis: axis(),
+        yaxis: axis(),
+        xaxis2: axis(),
+        yaxis2: axis(),
+    };
+
+    Plotly.newPlot(name, data, layout);
+}
