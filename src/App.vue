f94b83c5cdd0005cd724736a2df8576942456844
yunaTseng <fav0124@gmail.com>
2022-09-13 00:42:41 +0800

    FIX: 初版

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@
       >
         <v-card-text class="white--text">
           <ol>
-            <li v-for="(citation, i) in citeList" :key="i">
+            <!-- <li v-for="(citation, i) in citeList" :key="i">
               <a
                 :id="`event_${i}`"
                 :tabindex="i"
@@ -43,7 +43,7 @@
               >
                 {{ citation.name }}
               </a>
-            </li>
+            </li> -->
           </ol>
         </v-card-text>
 
@@ -72,10 +72,10 @@ export default {
     Main,
   },
   data: () => ({
-     citeList: [{ url: 'https://zh.wikipedia.org/w/index.php?title=%E5%B1%8F%E6%9D%B1%E6%8C%96%E7%9C%BC%E6%A1%88&oldid=69168796', name: '屏東挖眼案. (2021, December 19). Retrieved from 維基百科, 自由的百科全書' },
-      { url: 'https://www.ptt.cc/bbs/Gossiping/M.1633226685.A.2D7.html', name: '[問卦] 幹你娘，屏東挖眼案通通給我洗起來！' },
-      { url: 'https://www.ptt.cc/bbs/Gossiping/M.1633333606.A.5F0.html', name: 'PTT 網友統計資料' },
-       { url: 'https://www.freepik.com/vectors/woman-drawing', name: 'Woman drawing vector created by rawpixel.com - www.freepik.com' }]
+    //  citeList: [{ url: 'https://zh.wikipedia.org/w/index.php?title=%E5%B1%8F%E6%9D%B1%E6%8C%96%E7%9C%BC%E6%A1%88&oldid=69168796', name: '屏東挖眼案. (2021, December 19). Retrieved from 維基百科, 自由的百科全書' },
+    //   { url: 'https://www.ptt.cc/bbs/Gossiping/M.1633226685.A.2D7.html', name: '[問卦] 幹你娘，屏東挖眼案通通給我洗起來！' },
+    //   { url: 'https://www.ptt.cc/bbs/Gossiping/M.1633333606.A.5F0.html', name: 'PTT 網友統計資料' },
+    //    { url: 'https://www.freepik.com/vectors/woman-drawing', name: 'Woman drawing vector created by rawpixel.com - www.freepik.com' }]
   }),
 };
 </script>
